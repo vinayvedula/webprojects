bafa697f47a3c1ba6638feaf18a7e70b89b322d8
vinayvedula <vinaykumar_vedula@yahoo.co.in>
2022-07-23 22:48:48 -0500

    Initial Project Commit 1

---
+++ src/main/java/com/vinay/pws/PwsApplication.java
@@ -0,0 +1,13 @@
+package com.vinay.pws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinay/pws/PwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.pws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
